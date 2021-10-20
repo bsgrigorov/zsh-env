70f6e0b780380cb1f83d952b67ce68b91fd2c8dd
Borislav Grigorov <b.s.grigorov@gmail.com>
2021-10-19 21:08:25 -0700

    Added alias

--- work.sh
+++ work.sh
@@ -13,7 +13,7 @@ k8s["uat"]="kubeconfig--eureka--uat.yaml"
 k8s["black"]="kubeconfig--eureka--black.yaml"
 k8s["jira"]="kubeconfig--eureka--jira.yaml"
 k8s["e2e"]="kubeconfig--eureka--e2e.yaml"
-k8s["staging"]="kubeconfig--eureka--stagin-1.yaml"
+k8s["staging"]="kubeconfig--eureka--staging-1.yaml"
 k8s["trial"]="kubeconfig--eureka--trial.yaml"
 
 k8s["gke-ci"]="kubeconfig--eureka--gke-ci.yaml"
