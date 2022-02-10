564eef5fb536bb59a59cd9a3617973141ca8eb55
Borislav Grigorov <b.s.grigorov@gmail.com>
2022-02-09 22:10:22 -0800

    Added daily and instant cron scripts

---
+++ tasks/instant.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PATH=$PATH':/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+source ~/.zshrc
+date
+
+# list crontabs - crontab -l
+# edit crontabs - crontab -e
+
+# shallow-backup --new-path ~/Development/Repos/home/backup -backup-all 
